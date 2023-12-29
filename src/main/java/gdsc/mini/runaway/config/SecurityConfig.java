810c0ddff0d61473098a1cdc694cec117652221c
lizuAg <wndms058@gmail.com>
2023-12-30 03:09:09 +0900

    add SecurityConfig

--- src/main/java/gdsc/mini/runaway/config/SecurityConfig.java
+++ src/main/java/gdsc/mini/runaway/config/SecurityConfig.java
@@ -2,18 +2,13 @@ package gdsc.mini.runaway.config;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().antMatchers(
-                "/v3/api-docs/**", "/h2-console/**"
-        );
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,11 +17,8 @@ public class SecurityConfig {
 
         http.authorizeRequests()
                 .antMatchers("/login").permitAll()
-                .antMatchers("/v3/api-docs/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html").permitAll()
+                .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/swagger-ui.html").permitAll()
                 .anyRequest().authenticated();
-
         return http.build();
     }
 }
